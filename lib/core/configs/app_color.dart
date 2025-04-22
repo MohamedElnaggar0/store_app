930d308b7145ae95356392e84020ba806e123f84
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-04-22 17:49:09 +0200

    create dark and light theme and add font

---
+++ lib/core/configs/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryColor = Color(0xFF8E6CEF);
+  static const Color darkSecondBackgroundColor = Color(0xFF342F3F);
+  static const Color darkBackgroundColor = Color(0xFF1D182A);
+  static const Color lightSecondBackgroundColor = Color(0xFFF4F4F4);
+  static const Color lightBackgroundColor = Color(0xFFFFFFFF);
+}
---
+++ lib/core/configs/app_theme.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:store_app/core/configs/app_color.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: AppColor.primaryColor,
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: AppColor.lightBackgroundColor,
+    fontFamily: 'CircularStd',
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: AppColor.lightSecondBackgroundColor,
+      hintStyle: const TextStyle(
+          color: Color(0xFF8E8E8E), fontWeight: FontWeight.w500),
+      contentPadding: EdgeInsets.all(30),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide.none,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide.none,
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide.none,
+      ),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    primaryColor: AppColor.primaryColor,
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColor.darkBackgroundColor,
+    fontFamily: 'CircularStd',
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: AppColor.darkSecondBackgroundColor,
+      hintStyle: const TextStyle(
+          color: Color(0xFFA7A7A7), fontWeight: FontWeight.w500),
+      contentPadding: EdgeInsets.all(30),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide.none,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide.none,
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide.none,
+      ),
+    ),
+  );
+}

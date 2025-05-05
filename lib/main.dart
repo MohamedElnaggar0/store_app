9ffbbef7eb12be52c1139b546a1491ba170b6d6f
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-05-05 16:14:58 +0300

    firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_app/core/configs/app_router.dart';
 import 'package:store_app/core/configs/theme/app_theme.dart';
 import 'package:store_app/features/splash/presentation/bloc/cubit/splash_cubit.dart';
+import 'package:store_app/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const StoreApp());
 }
 

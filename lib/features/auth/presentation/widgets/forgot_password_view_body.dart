c099ac5c8199f081e672640b5959c41e9b54ffc4
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-05-22 15:53:30 +0300

    handel the logic in ui done

--- lib/features/auth/presentation/widgets/forgot_password_view_body.dart
+++ lib/features/auth/presentation/widgets/forgot_password_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:store_app/core/configs/router/app_router.dart';
+import 'package:store_app/features/auth/domain/usecases/change_password_usecase.dart';
+import 'package:store_app/features/auth/presentation/blocs/button_cubit/button_cubit.dart';
 import 'package:store_app/features/auth/presentation/widgets/custom_button.dart';
 import 'package:store_app/features/auth/presentation/widgets/custom_text.dart';
 import 'package:store_app/features/auth/presentation/widgets/custom_text_field.dart';
@@ -10,6 +13,7 @@ class ForgotPasswordViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController emailController = TextEditingController();
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
       child: Column(
@@ -22,15 +26,38 @@ class ForgotPasswordViewBody extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
           const SizedBox(height: 20),
-          const CustomTextField(
+          CustomTextField(
             hintText: 'Enter Email Address',
+            txtController: emailController,
           ),
           const SizedBox(height: 20),
-          CustomButton(
-            text: 'Continue',
-            onTap: () {
-              GoRouter.of(context).push(AppRouter.kSendToEmailView);
-            },
+          BlocProvider(
+            create: (context) => ButtonCubit(),
+            child: BlocListener<ButtonCubit, ButtonState>(
+              listener: (context, state) {
+                if (state is ButtonSuccess) {
+                  GoRouter.of(context).push(AppRouter.kSendToEmailView);
+                }
+              },
+              child: BlocBuilder<ButtonCubit, ButtonState>(
+                builder: (context, state) {
+                  if (state is ButtonLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (state is ButtonFailure) {
+                    return Center(child: Text(state.message));
+                  }
+                  return CustomButton(
+                    text: 'Continue',
+                    onTap: () {
+                      context.read<ButtonCubit>().clickButton(
+                          params: emailController.text,
+                          usecase: ChangePasswordUsecase());
+                    },
+                  );
+                },
+              ),
+            ),
           ),
         ],
       ),
